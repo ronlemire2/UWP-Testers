927e2ba5f647283faa19386bc0b64ae0f2a47f03
ronlemire2 <ronlemire2@gmail.com>
2016-01-24 18:54:47 -0800

    Added FileIOTester.

---
+++ FileIOTester/FileIOTester/Attributes/LaterThanPropertyAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOTester.Attributes {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class LaterThanPropertyAttribute : ValidationAttribute {
+        string propertyName;
+
+        public LaterThanPropertyAttribute(string propertyName) {
+            this.propertyName = propertyName;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext obj) {
+            if (value == null) {
+                return ValidationResult.Success;
+            }
+
+            var propertyInfo = obj.ObjectInstance.GetType().GetProperty(this.propertyName);
+
+            if (propertyInfo == null) {
+                // Should actually throw an exception.
+                return ValidationResult.Success;
+            }
+
+            dynamic otherValue = propertyInfo.GetValue(obj.ObjectInstance);
+
+            if (otherValue == null) {
+                return ValidationResult.Success;
+            }
+
+            // Unfortunately we have to use the DateTime type here.
+            //var compare = ((IComparable<DateTime>)otherValue).CompareTo((DateTime)value);
+            var compare = otherValue.CompareTo((DateTime)value);
+
+            if (compare >= 0) {
+                return new ValidationResult(this.ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ FileIOTester/FileIOTester/ViewModels/BasicSubPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Prism.Windows.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIOTester.ViewModels {
+    public class BasicSubPageViewModel : ViewModelBase {
+    }
+}
